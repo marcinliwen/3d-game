8b73ab44886a8f9f3ce8083e2aec9ef38dbd7866
marcinliwen <liwen.marcin@gmail.com>
2018-12-11 23:53:00 +0100

    add "check" button unctionality

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 window.onload = () =>{
     
-    const keys =document.getElementsByClassName('key');    
+    const keys = document.getElementsByClassName('key');    
+    const counterContener = document.querySelector('.__counter');
+   
+    var countRounds = 0;
+
 
     //set user output
     var userOutput = document.querySelector('.user__input'); 
@@ -17,11 +21,30 @@ window.onload = () =>{
             if( j < 4){
                 replaceX(newValue, j);
                 j++;  
-            }            
-        })
-        
+            }  
+            if(j > 3){ //if user input 3 digits enable button to check
+                document.querySelector('.check').disabled = false;  
+            }                 
+        })  
     }
     
+    
+    
+    //event listener for checking number - next round
+    document.querySelector('.check').addEventListener('click', function(){
+        var currentRound = countMyRound();
+        //there is only 10 round in Game
+        if(currentRound < 11){
+            displayCurrentRound(currentRound);
+        }else{
+            gameOver();
+        };
+        //disable check button
+        this.disabled = true;
+        //reset values
+        resetValues();
+    })
+
     //replace x with user input
     function replaceX(newValue, j){       
         var newOutput = userOutput.innerHTML; //current output                
@@ -30,15 +53,42 @@ window.onload = () =>{
     };
     
     
-    var reste = document.querySelector('.reset');
+    var reset = document.querySelector('.reset');
     
     //reset vlues
-    reste.addEventListener('click', function(){
+    reset.addEventListener('click', function(){
+        
+    })
+
+    function resetValues(){
         userOutput.innerHTML = "XXX";
         j = 1;
         newValue = "";
-    })
+    }
+    //count game
+    function countMyRound(){
+       return countRounds += 1;
+    }
+
+    //display current game round
+    function displayCurrentRound(num){        
+        
+        if (counterContener.hasChildNodes()) {
+            counterContener.removeChild(counterContener.childNodes[0]);
+        }
+        var text = document.createTextNode(num);
+        var p = document.createElement("P");
+        p.appendChild(text);
+        counterContener.appendChild(p);            
+    }
+
+
+    //game over
+    function gameOver(){
+        alert('game over')
+    }
 }
+
 
 
    
